88e4b650ffb72116393a1d5e4b2ae3218069befe
seokjoon <seokjoon@gmail.com>
2022-12-02 10:32:45 +0900

    touch

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,9 +41,4 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
-    public function routeNotificationForSlack($notification)
-    {
-        return `http://hooks.slack.com/services/...`;
-    }
 }
